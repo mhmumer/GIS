f46d58ab4aae2266ab4a60ce4122bc9f580e6c25
Muhammad Umer <rawalaumer@gmail.com>
2024-10-16 15:40:06 +0500

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ import Logo from "@/public/footer.png";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-100 text-gray-900">
-      <div className="container mx-auto px-4 py-8 grid grid-cols-1 md:grid-cols-4 gap-8">
+      <div className="container mx-auto  py-8 grid grid-cols-1 md:grid-cols-4 gap-8">
         {/* Company Info */}
         <div>
           <Image
